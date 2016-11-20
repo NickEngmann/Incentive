a555227cf4e1e92ae5284ed82740b76faf67146a
NickySlicksHaha <nickengmann_2@yahoo.com>
2016-11-19 19:56:34 -0500

    everything but backend connection

--- extension/incentive.js
+++ extension/incentive.js
@@ -1,39 +1,57 @@
+var goalDate = 23;
+var goalMonth = 6;
+var goalYear = 2023;
+var goalText = "Click the Logo and Set a Goal";
+
+var birthdayDate = 23;
+var birthdayMonth = 6;
+var birthdayYear = 1993;
+
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
-
+  var years = 0;
+  if (days > 365){
+    years = Math.floor(days/365);
+    days = days%365;
+  }
   return {
     'total': t,
     'days': days,
     'hours': hours,
     'minutes': minutes,
-    'seconds': seconds
+    'seconds': seconds,
+    'years' : years
   };
 }
 
-function initializeClock(id, endtime) {
+function initializeClock(id, endtime, goaltime) {
   var clock = document.getElementById(id);
-  // var daysSpan = clock.querySelector('.days');
+  var daysSpan = clock.querySelector('.days');
   var hoursSpan = clock.querySelector('.hours');
   var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
   var secondsinDaySpan = clock.querySelector('.secondsinDay');
   var ageSpan = clock.querySelector('.age');
+  var yearsSpan = clock.querySelector('.years');
+  
+  var goalSpan = clock.querySelector('.goals');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    // console.log(t.hours);
-    // console.log(t.minutes);
-    // console.log(t.seconds);
-    // daysSpan.innerHTML = t.days;
+    var l = getTimeRemaining(goaltime);
+
     var current = new Date();
     var age = (current - birthday)/31557600000;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    yearsSpan.innerHTML = l.years;
+    daysSpan.innerHTML = l.days;
+    goalSpan.innerHTML = goalText;
+    hoursSpan.innerHTML = ('0' + l.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + l.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + l.seconds).slice(-2);
     var secondsinDay = ((t.minutes)*60 + (t.hours)*60*60 + t.seconds);
     secondsinDaySpan.innerHTML = ('0000' + secondsinDay).slice(-5);
     ageSpan.innerHTML = age.toPrecision(12);
@@ -46,12 +64,18 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 100);
 }
 
-	var birthday = new Date("June 23, 1993 01:00:00");
+	var birthday = new Date();
+  birthday.setYear(birthdayYear);
+  birthday.setMonth(birthdayMonth);
+  birthday.setDate(birthdayDate);
+  
  	var d = new Date();
  	var h = d.getHours();
  	var m = d.getMinutes();
  	var s = d.getSeconds();
-// 	secondsInDay = (24*60*60) - (h*60*60) - (m*60) - s;
-	
 	var deadline = new Date(Date.parse(new Date()) + 1 * (24 * 60 * 60 - (60*m + s) - (h*60*60)) * 1000);
-	initializeClock('clockdiv', deadline);
+	var goal = new Date();  
+  goal.setYear(goalYear);
+  goal.setMonth(goalMonth);
+  goal.setDate(goalDate);
+  initializeClock('clockdiv', deadline, goal);
